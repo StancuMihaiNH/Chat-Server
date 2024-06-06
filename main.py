6917d67496f8ac74ca6b04049579623a2ee0b39d
Stancu Mihai Cristian <mihai.stancu@northhighland.com>
2024-06-06 07:22:06 -0700

    Added a dummy endpoint

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+from flask import Flask
+
+app = Flask(__name__)
 
 
-# Press the green button in the gutter to run the script.
+@app.route('/')
+def hello():
+    return 'Hello, world!'
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm ')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run()
